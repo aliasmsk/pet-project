734cbcbfcb8a0ced8fa2939965205b58a9ed8ffc
aliasmsk <alienon597@gmail.com>
2026-09-06 17:33:51 +0000

    feat: test cd

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def read_root():
 
 @app.get("/db-check")
 def check_db():
-    """Эндпоинт, который пробует подключиться к PostgreSQL"""
+    """Эндипоинт, который пробует подключиться к PostgreSQL"""
     try:
         conn = psycopg2.connect(
             host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
